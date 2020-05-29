2f0bc29d560f44037cad2ea073a1597e32ac2172
1191631 <1191631@isep.ipp.pt>
2020-05-29 17:56:18 +0100

    Sprint 1 - Implementation Start [Documentation]

---
+++ src/main/java/pt/ipp/isep/dei/esoft/pot/model/Manager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Universidade
+ */
+public class Manager {
+    
+    private String name;
+    private String email;
+    private String password;
+    
+    public Manager (String name,String email,String password ){
+        this.name = name;
+        this.email = email;
+        this.password = email;
+    }
+}
